eb78c91095b428f8882549709f8511a613ac8134
Luka Vercauteren <l@hoefman.ch>
2024-10-11 16:17:49 +0200

    Started adding function signatures

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from exif import Image
+import datetime
+
+def open_image(path: str) -> Image:
+    pass
+
+def get_time_from_image_file(path: str) -> datetime.datetime:
+    pass
